9a75548cdadd611859fdf63d13b5c3109912de08
Nomad <noneprado@gmail.com>
2017-07-31 10:40:46 +0600

    Minor refactor

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -5,23 +5,8 @@ const Schema = mongoose.Schema;
 
 
 const TaskSchema = new Schema({
-  name: {
-    type: String,
-    Required: 'Kindly enter the name of the task'
-  },
-  Created_date: {
-    type: Date,
-    default: Date.now
-  },
-  status: {
-      type: [
-          {
-            type: String,
-            enum: ['pending', 'ongoing', 'completed']
-          }
-      ],
-    default: ['pending']
-  }
+    text: String,
+    done: Boolean
 });
 
 module.exports = mongoose.model('Tasks', TaskSchema);
