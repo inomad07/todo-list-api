8621e2ddd3b046fa145c6193277a92fcbc9c55f3
nomad <noneprado@gmail.com>
2018-09-18 00:39:37 +0600

    in progress

--- resources/TodoResource.js
+++ resources/TodoResource.js
@@ -7,28 +7,24 @@ class TodoResource {
         console.log('getAll: ', req);
 
         TodoModel.find({}, (err, todo) => {
-            if (err){
+            if (err)
                 res.send(err);
-            }
 
             res.json(todo);
         })
-            .then(todos => res.status(200).json(clientResponse.sendSuccessMsg('ok', 'Todo list successfully received', todos)))
-            .catch(err => res.status(500).json(clientResponse.sendErrorMsg(err)));
+
     }
 
 
     get(req, res) {
         let todoId = req.params.todoId;
         TodoModel.findById(todoId, (err, todo) => {
-            if (err) {
+            if (err)
                 res.send(err);
-            }
 
             res.json(todo);
         })
-            .then(todo => res.status(200).json(clientResponse.sendSuccessMsg('ok', 'Todo successfully received', todo)))
-            .catch(err => res.status(500).json(clientResponse.sendErrorMsg(err)));
+
     }
 
     create(req, res) {
@@ -36,28 +32,24 @@ class TodoResource {
         let params = req.body;
         let todoModel = new TodoModel(params);
         todoModel.save( (err, todo) => {
-            if (err) {
+            if (err)
                 res.send(err);
-            }
+
 
             res.json(todo);
         })
-            .then(todo => res.status(200).json(clientResponse.sendSuccessMsg('ok', 'Todo successfully created', todo)))
-            .catch(err => res.status(500).json(clientResponse.sendErrorMsg(err)));
+
     }
 
 
     update(req, res) {
         let todoId =  req.params.todoId;
         TodoModel.findOneAndUpdate({_id: todoId}, req.body, {new: true}, (err, todo) => {
-            if (err) {
+            if (err)
                 res.send(err);
-            }
 
             res.json(todo);
         })
-            .then(todo => res.status(200).json(clientResponse.sendSuccessMsg('ok', 'Todo successfully updated', todo)))
-            .catch(err => res.status(500).json(clientResponse.sendErrorMsg(err)));
     }
 
     editTodo() {
@@ -67,8 +59,6 @@ class TodoResource {
                 todo.toggle = !todo.toggle;
                 return todo.save();
             })
-            .then(todo => res.status(200).json(clientResponse.sendSuccessMsg('ok', 'Todo successfully edited', todo)))
-            .catch((err) => res.status(500).json(err));
     }
 
     delete(req, res) {
@@ -76,14 +66,11 @@ class TodoResource {
         TodoModel.remove({
             _id: todoId
         }, (err, todo) => {
-            if (err) {
+            if (err)
                 res.send(err);
-            }
 
             res.json(todo);
         })
-            .then(todo => res.status(200).json(clientResponse.sendSuccessMsg('ok', 'Todo successfully removed', todo)))
-            .catch((err) => res.status(500).json(err));
     }
 }
 
