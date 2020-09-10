068b38b4e3a9b13cf6919fdd340b0e82dcd8acc1
Nomad <noneprado@gmail.com>
2020-09-10 16:28:35 +0300

    hot fix

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const mongoConfig = require('../utils/Helper');
+const mongoConfig = require("../utils/helper");
 const env = process.env.ENV;
 const mongoPwd = process.env.MONGO_PWD;
 const mongoUser = process.env.MONGO_USER;
@@ -10,7 +10,7 @@ const pwd = mongoPwd.replace(/@/g, "\\@");
 const userUri = `${mongoUser}:${pwd}`;
 const hostUri = mongoConfig.getConnectionUri();
 const port = process.env.MONGO_PORT;
-const dbUri =  process.env.MONGO_DB;
+const dbUri = process.env.MONGO_DB;
 const options = mongoConfig.getOptionUri();
 
 let mongoConnectionUri;
@@ -18,15 +18,15 @@ let mongoConnectionUri;
 let mongooseConfig = {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 };
 
-if (env === 'production') {
+if (env === "production") {
     mongoConnectionUri = `mongodb://${userUri}@${hostUri}/${dbUri}?${options}`;
-    console.log('This project is running in production mode');
+    console.log("This project is running in production mode");
 } else {
     mongoConnectionUri = `mongodb://${hostUri}/${dbUri}`;
-    console.log('This project is running in development mode');
+    console.log("This project is running in development mode");
 }
 
 //Configure mongoose's promise to global promise
@@ -36,13 +36,12 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(mongoConnectionUri, mongooseConfig);
         console.log(`MongoDB Connected: ${conn.connection.host}:${port}`);
-    }
-    catch (err) {
-        console.log(`MongoDB connection error ${err}`)
+    } catch (err) {
+        console.log(`MongoDB connection error ${err}`);
     }
 };
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('debug', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("debug", true);
 
 module.exports = connectDB;
